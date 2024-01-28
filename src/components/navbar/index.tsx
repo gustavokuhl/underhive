65c7886a30b8cbe87b9304dcf7d26583bf146e5b
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-01-28 13:52:03 -0300

    style: update app name

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,7 +4,7 @@ import UserAvatar from "../user-avatar"
 export default function Navbar() {
   return (
     <div className="flex justify-between border-b py-2 items-end">
-      <p className="text-3xl font-semibold">UnderHive</p>
+      <p className="text-3xl font-semibold">SkateNextApp</p>
       <div className="flex items-center gap-2">
         <UserAvatar
           src="https://ipfs.skatehive.app/ipfs/Qmdka9mpMbrZnEDwh7VaAX13vkMKyqu21Jp3QQXwZtnGeR?pinataGatewayToken=nxHSFa1jQsiF7IHeXWH-gXCY3LDLlZ7Run3aZXZc8DRCfQz4J4a94z9DmVftXyFE"
